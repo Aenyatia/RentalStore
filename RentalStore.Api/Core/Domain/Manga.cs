48e2d89d0ae6d0850d1465022356a59ad998e4e5
Aenyatia <aenyatia@gmail.com>
2018-09-16 17:02:55 +0200

    Add manga model.

---
+++ RentalStore.Api/Core/Domain/Manga.cs
@@ -0,0 +1,12 @@
+﻿namespace RentalStore.Api.Core.Domain
+{
+	public class Manga
+	{
+		public int MangaId { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Genre { get; set; }
+		public decimal Price { get; set; }
+		public int InStock { get; set; }
+	}
+}
