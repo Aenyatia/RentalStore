97892c03900ff348a444043f4bed5af3c25fce0a
Aenyatia <aenyatia@gmail.com>
2018-09-17 13:44:25 +0200

    Add order model.

---
+++ RentalStore.Api/Core/Domain/Address.cs
@@ -0,0 +1,8 @@
+﻿namespace RentalStore.Api.Core.Domain
+{
+	public class Address
+	{
+		public string Street { get; set; }
+		public string City { get; set; }
+	}
+}
---
+++ RentalStore.Api/Core/Domain/OrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace RentalStore.Api.Core.Domain
+{
+	public class OrderItem
+	{
+		public Product Product { get; set; }
+		public decimal Price { get; set; }
+		public int Quantity { get; set; }
+	}
+}
